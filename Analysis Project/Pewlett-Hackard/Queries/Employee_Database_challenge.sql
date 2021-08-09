f4aa0c5461ca27236430c2b12b407fe918e6a46e
Enrico Robles del Rio <enrico_rr@icloud.com>
2021-08-08 21:12:02 -0500

    Adding deliverable I

---
+++ Analysis Project/Pewlett-Hackard/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,32 @@
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT e.emp_no, e.first_name, e.last_name, 
+ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, ti.to_date DESC;
+
+-- Unique Titles 
+SELECT DISTINCT ON (ret.emp_no) ret.emp_no, ret.first_name, ret.last_name, ret.title
+INTO unique_titles
+FROM retirement_titles as ret
+ORDER BY ret.emp_no, ret.to_date DESC;
+
+-- Employee Count / Title / Retirement 
+SELECT COUNT(ut.title), ut.title
+INTO retirement_titles_count
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+
+
+
+
+
+
+
+
+
+
